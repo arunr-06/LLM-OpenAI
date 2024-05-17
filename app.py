8cb94af86fc19e41dff35b9d4ce45a81110cca45
Arun <arunkumariimr@gmail.com>
2024-05-17 20:34:28 +0800

    Third commit

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores.faiss import FAISS
